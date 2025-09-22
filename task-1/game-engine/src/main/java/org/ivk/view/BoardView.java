9e8a384f1c1c194359e22d4221e242450184f568
gKonstantin <gkonstantin17@ya.ru>
2025-09-22 23:00:06 +0300

    render board

--- task-1/game-engine/src/main/java/org/ivk/view/BoardView.java
+++ task-1/game-engine/src/main/java/org/ivk/view/BoardView.java
@@ -3,11 +3,37 @@ package org.ivk.view;
 import org.ivk.entity.Board;
 
 public class BoardView {
+    private final String W = "●";
+    private final String B = "○";
+    private final String EMPTY = "·";
     private Board board;
     public BoardView(Board board) {
         this.board = board;
     }
     public void printBoard() {
-        board.getField();
+        int[][] field = board.getField();
+        int size = board.getSize();
+
+        // Заголовок с номерами столбцов
+        System.out.print("   ");
+        for (int col = 0; col < size; col++) {
+            System.out.printf("%2d ", col);
+        }
+        System.out.println();
+
+        // Само поле
+        for (int row = 0; row < size; row++) {
+            System.out.printf("%2d ", row); // Номер строки
+            for (int col = 0; col < size; col++) {
+                String symbol;
+                switch (field[row][col]) {
+                    case 2 -> symbol = W;
+                    case 1 -> symbol = B;
+                    default -> symbol = EMPTY;
+                }
+                System.out.printf(" %s ", symbol);
+            }
+            System.out.println();
+        }
     }
 }

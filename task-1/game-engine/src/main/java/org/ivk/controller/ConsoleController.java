8561f75912e11b885aee0dec230c517009b87b64
gKonstantin <gkonstantin17@ya.ru>
2025-09-22 22:59:42 +0300

    add more names for commands

--- task-1/game-engine/src/main/java/org/ivk/controller/ConsoleController.java
+++ task-1/game-engine/src/main/java/org/ivk/controller/ConsoleController.java
@@ -17,16 +17,16 @@ public class ConsoleController {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String message = scanner.nextLine();
-            if (message.startsWith("GAME") || message.startsWith("game"))
+            if (message.startsWith("GAME") || message.startsWith("game") || message.startsWith("Game"))
                 gameService.receiveCommand(message);
 
-            else if (message.startsWith("MOVE") || message.startsWith("move"))
+            else if (message.startsWith("MOVE") || message.startsWith("move") || message.startsWith("Move"))
                 gameService.receiveCommand(message);
 
-            else if (message.equals("HELP") || message.equals("help"))
+            else if (message.equals("HELP") || message.equals("help") || message.equals("Help"))
                 messageView.help();
 
-            else if (message.equals("EXIT") || message.equals("exit")) {
+            else if (message.equals("EXIT") || message.equals("exit") || message.equals("Exit")) {
                 scanner.close();
                 break;
             }

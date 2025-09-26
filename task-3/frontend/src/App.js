8c526db0b1c000da7af4b3ceaa4055f8427750c4
gKonstantin <gkonstantin17@ya.ru>
2025-09-26 10:46:14 +0300

    put components together

--- task-3/frontend/src/App.js
+++ task-3/frontend/src/App.js
@@ -1,11 +1,30 @@
-import './style.css';
-// npm install @mui/material @emotion/react @emotion/styled
+import { useState } from "react";
+import GameForm from "./components/GameForm";
+import GameBoard from "./components/GameBoard";
+import ResultModal from "./components/ResultModal";
+import Help from "./components/Help";
+import "./style.css";
+
 function App() {
-  return (
-    <div className="App">
-     ку
-    </div>
-  );
+    const [gameConfig, setGameConfig] = useState(null);
+    const [result, setResult] = useState(null);
+
+    return (
+        <div style={{ position: "relative", padding: 20, maxWidth: 900, margin: "0 auto" }}>
+            {!gameConfig ? (
+                <GameForm onGameStart={setGameConfig} />
+            ) : (
+                <GameBoard
+                    size={gameConfig.size}
+                    currentPlayer={{ type: gameConfig.player1Type, color: gameConfig.player1Color }}
+                    onMove={(res) => setResult(res.cmd || res.result || "Ход сделан")}
+                />
+            )}
+
+            <ResultModal open={!!result} result={result} onClose={() => setResult(null)} />
+            <Help />
+        </div>
+    );
 }
 
 export default App;
